589d5acad4a299d96af736b5c797698c1971df9a
Tiago Miguel Cunha <tiago.miguel.cunha@celfocus.com>
2022-05-10 23:34:53 +0100

    Change edit form on table items WIP

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Table as MuiTable } from "@material-ui/core";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { TableExpandableItem } from "./sytles";
-import { withStyles } from "@material-ui/core/styles";
-import { TextInput } from "./Inputs";
+import React, { useState } from 'react';
+import { Table as MuiTable } from '@material-ui/core';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp, faPen, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { TableExpandableItem } from './sytles';
+import { withStyles } from '@material-ui/core/styles';
+import { TextInput } from './Inputs';
 
 type ExpandableItemsProps = {
   open: boolean;
@@ -19,38 +19,82 @@ type ExpandableItemsProps = {
 function ExpandableItems(props: ExpandableItemsProps) {
   return (
     <>
-      <TableCell colSpan={5} style={{ padding: "0px" }}>
+      <TableCell colSpan={6} style={{ padding: '0px' }}>
         <TableExpandableItem>
           <p>June 2022</p>
-          {props.open ? (
-            <FontAwesomeIcon icon={faChevronDown} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronUp} />
-          )}
+          {props.open ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
         </TableExpandableItem>
       </TableCell>
     </>
   );
 }
 
-const Teste = () => {
-  const [open, setOpen] = React.useState(false);
+function TableItem() {
+  const [edit, setEdit] = useState(false);
 
-  const StyledTableCell = withStyles((theme) => ({
+  const StyledTableRow = withStyles(theme => ({
     root: {
-      fontFamily: "Smart Finance Regular",
-      fontSize: "14px",
-    },
-  }))(TableCell);
-
-  const StyledTableRow = withStyles((theme) => ({
-    root: {
-      "&:hover": {
-        background: "#f9f9f9",
-        cursor: "pointer",
+      '&:hover': {
+        background: '#f9f9f9',
+        cursor: 'pointer',
       },
     },
   }))(TableRow);
+  const StyledTableCell = withStyles(theme => ({
+    root: {
+      fontFamily: 'Smart Finance Regular',
+      fontSize: '14px',
+    },
+  }))(TableCell);
+
+  function handleEdit() {
+    setEdit(!edit);
+  }
+
+  return (
+    <>
+      <StyledTableRow>
+        {!edit ? (
+          <>
+            <StyledTableCell>11/06/1992</StyledTableCell>
+            <StyledTableCell>Volvo</StyledTableCell>
+            <StyledTableCell>Carro</StyledTableCell>
+            <StyledTableCell>FixExpenses</StyledTableCell>
+            <StyledTableCell>Volvo</StyledTableCell>
+            <StyledTableCell>
+              <FontAwesomeIcon icon={faPen} onClick={handleEdit} />
+            </StyledTableCell>
+          </>
+        ) : (
+          <>
+            <StyledTableCell>
+              <TextInput text='Volvo' />
+            </StyledTableCell>
+            <StyledTableCell>
+              <TextInput text='Volvo' />
+            </StyledTableCell>
+            <StyledTableCell>
+              <TextInput text='Volvo' />
+            </StyledTableCell>
+            <StyledTableCell>
+              <TextInput text='Volvo' />
+            </StyledTableCell>
+            <StyledTableCell>
+              <TextInput text='Volvo' />
+            </StyledTableCell>
+            <StyledTableCell>
+              <FontAwesomeIcon icon={faCheck} onClick={handleEdit} />
+              <FontAwesomeIcon icon={faXmark} onClick={handleEdit} />
+            </StyledTableCell>
+          </>
+        )}
+      </StyledTableRow>
+    </>
+  );
+}
+
+const Teste = () => {
+  const [open, setOpen] = React.useState(false);
 
   return (
     <>
@@ -60,52 +104,30 @@ const Teste = () => {
 
       {open && (
         <>
-          <StyledTableRow>
-            <StyledTableCell>11/06/1992</StyledTableCell>
-            <StyledTableCell>
-              <TextInput text="Volvo" />
-            </StyledTableCell>
-            <StyledTableCell>Carro</StyledTableCell>
-            <StyledTableCell>FixExpenses</StyledTableCell>
-            <StyledTableCell>
-              <TextInput text="Volvo" />
-            </StyledTableCell>
-          </StyledTableRow>
-          <StyledTableRow>
-            <StyledTableCell>11/06/1992</StyledTableCell>
-            <StyledTableCell>Prestacao Volvo</StyledTableCell>
-            <StyledTableCell>Carro</StyledTableCell>
-            <StyledTableCell>FixExpenses</StyledTableCell>
-            <StyledTableCell>200$</StyledTableCell>
-          </StyledTableRow>
-          <StyledTableRow>
-            <StyledTableCell>11/06/1992</StyledTableCell>
-            <StyledTableCell>Prestacao Volvo</StyledTableCell>
-            <StyledTableCell>Carro</StyledTableCell>
-            <StyledTableCell>FixExpenses</StyledTableCell>
-            <StyledTableCell>200$</StyledTableCell>
-          </StyledTableRow>
+          <TableItem />
+          <TableItem />
+          <TableItem />
         </>
       )}
     </>
   );
 };
 
-const StyledTable = withStyles((theme) => ({
+const StyledTable = withStyles(theme => ({
   root: {},
 }))(MuiTable);
 
-const StyledTableCell = withStyles((theme) => ({
+const StyledTableCell = withStyles(theme => ({
   root: {
-    fontFamily: "Smart Finance Bold",
-    fontSize: "18px",
+    fontFamily: 'Smart Finance Bold',
+    fontSize: '18px',
   },
 }))(TableCell);
 
 export function Table() {
   return (
     <TableContainer component={Paper}>
-      <StyledTable aria-label="collapsible table">
+      <StyledTable aria-label='collapsible table'>
         <TableHead>
           <TableRow>
             <StyledTableCell>Date</StyledTableCell>
@@ -113,6 +135,7 @@ export function Table() {
             <StyledTableCell>Category</StyledTableCell>
             <StyledTableCell>Tags</StyledTableCell>
             <StyledTableCell>Amount</StyledTableCell>
+            <StyledTableCell></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
